1a3cc12eb1d8a8eec82e0b51fadb7a31988381fd
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-02-16 02:26:04 +0100

    Fix webpack build

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,12 +1,6 @@
-const SentryCliPlugin = require('@sentry/webpack-plugin'); // XXX https://github.com/getsentry/sentry-webpack-plugin
 const slsw = require('serverless-webpack'); // XXX https://github.com/serverless-heaven/serverless-webpack
 const nodeExternals = require('webpack-node-externals');
 const path = require('path');
-const webpack = require('webpack');
-const GitRevisionPlugin = require('git-revision-webpack-plugin'); // XXX https://www.npmjs.com/package/git-revision-webpack-plugin
-const moment = require('moment');
-
-const gitRevisionPlugin = new GitRevisionPlugin();
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -36,24 +30,6 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    // XXX We need to overwrite NODE_ENV because WEBPACK allows only two stages "production" and "development" which causes issues with our "staging" environment
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.ENV),
-      'process.env.GIT_COMMIT_VERSION': JSON.stringify(gitRevisionPlugin.version()),
-      'process.env.DEPLOY_TIME': JSON.stringify(moment().format('LLLL')),
-    }),
-    new SentryCliPlugin({
-      include: '.',
-      ignoreFile: '.sentryclirc',
-      ignore: ['node_modules', 'webpack.config.js', 'coverage'],
-      release: gitRevisionPlugin.version(), // Dynamically get the latest git commit and use it as a release version
-      dryRun: slsw.lib.webpack.isLocal, // XXX dryRun is true when we work locally, avoid sending source files to Sentry each time the server is started locally.
-      configFile: './sentryclirc' // path to Sentry CLI config properties,
-      // as described in https://docs.sentry.io/learn/cli/configuration/#properties-files
-      // By default, the config file is looked for upwards from the current path and defaults
-      // from ~/.sentryclirc are always loaded
-    }),
-  ],
+  plugins: [],
 };
 
