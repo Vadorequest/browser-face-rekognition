70c5c51008ee84ae8748c95e9c6b8bbc71ccf8ce
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-02-16 04:00:00 +0100

    Working request with image_url

--- server/src/api.js
+++ server/src/api.js
@@ -1,19 +1,30 @@
 const fetch = require('node-fetch');
 
 export const handler = async (event, context) => {
+  console.log(event)
   const imageUrl = event.queryStringParameters.image_url;
+  const imageBase64 = event.queryStringParameters.image_base64;
+  const imageFile = event.queryStringParameters.image_file;
   const credentials = require('../api-credentials.json');
   var params = {
     api_key: credentials.API_KEY,
     api_secret: credentials.API_SECRET,
-    image_url: imageUrl,
     return_attributes: 'smiling',
   };
+
+  if (imageUrl) {
+    params.image_url = imageUrl;
+  } else if (imageBase64) {
+    params.image_base64 = imageBase64;
+  } else if (imageFile) {
+    params.image_file = imageFile;
+  }
+
+  console.log(JSON.stringify(params, null, 2), 'params')
 
   const searchParams = Object.keys(params).map((key) => {
     return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
   }).join('&');
-  console.log(searchParams);
 
   return await fetch('https://api-us.faceplusplus.com/facepp/v3/detect', {
     method: 'POST',
@@ -25,7 +36,6 @@ export const handler = async (event, context) => {
     },
   })
     .then(function (response) {
-      console.log(response);
       return response.json();
     })
     .then(function (json) {
